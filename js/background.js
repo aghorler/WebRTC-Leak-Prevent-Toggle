7686a7ade3b1570b53fe6e43a46c11e6bb32fadd
rentamob <aghorler@gmail.com>
2015-12-16 16:29:01 +1100

    Added new WebRTC options to toggle.
    webRTCNonProxiedUdpEnabled and webRTCIPHandlingPolicy now supported.

--- js/background.js
+++ js/background.js
@@ -7,23 +7,49 @@ chrome.runtime.onInstalled.addListener(function() {
 	chrome.browserAction.setIcon({
 		path: '/img/icon128_on.png'
 	});
+	chrome.privacy.network.webRTCIPHandlingPolicy.set({
+		value: 'default_public_interface_only'
+	});
 });
 
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-	try {
+	try { //Uses webRTCMultipleRoutesEnabled to toggle.
 		chrome.privacy.network.webRTCMultipleRoutesEnabled.get({}, function(setWebRTC) {
-			if (setWebRTC.value) {
+			if (setWebRTC.value) { //On.
+				//webRTCMultipleRoutesEnabled set to false.
 				chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
 					'value': false,
 					scope: 'regular'
 				});
+				//webRTCNonProxiedUdpEnabled set to user preference, same with webRTCIPHandlingPolicy.
+				chrome.storage.local.get(null, function(items) {
+					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+						value: !items.nonProxiedUDP,
+						scope: 'regular'
+					});
+					chrome.privacy.network.webRTCIPHandlingPolicy.set({
+					value: items.rtcIPHandling
+					});
+				});
 				chrome.browserAction.setIcon({
 					path: '/img/icon128_on.png'
 				});
-			} else {
+			} else { //Off.
+				//webRTCMultipleRoutesEnabled set to true.
 				chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
 					'value': true,
 					scope: 'regular'
+				}); 
+				
+				//webRTCNonProxiedUdpEnabled set to user preference. WebRTCIPHandlingPolicy set to 'default'.
+				chrome.storage.local.get(null, function(items) {
+					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+						value: items.nonProxiedUDP,
+						scope: 'regular'
+					});
+					chrome.privacy.network.webRTCIPHandlingPolicy.set({
+					value: 'default'
+					});
 				});
 				chrome.browserAction.setIcon({
 					path: '/img/icon128_off.png'
