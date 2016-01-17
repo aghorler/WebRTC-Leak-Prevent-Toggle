6f62d65db903c1b05465c4c31cb7bc8faa6a926a
rentamob <aghorler@gmail.com>
2016-01-18 00:01:32 +1100

    New options, major fixes.
    webRTCMultipleRoutesEnabled now an option.
    
    webRTCMultipleRoutesEnabled and webRTCNonProxiedUdpEnabled now presented
    as legacy options.
    
    Fixed an issue toggling webRTCNonProxiedUdpEnabled.
    
    Changed how default options are set on install.

--- js/background.js
+++ js/background.js
@@ -1,62 +1,91 @@
-/* On install, or update. Defaults to on. */
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-		'value': false,
-		scope: 'regular'
-	});
-	chrome.browserAction.setIcon({
-		path: '/img/icon128_on.png'
-	});
-	chrome.privacy.network.webRTCIPHandlingPolicy.set({
-		value: 'default_public_interface_only'
-	});
+/* Check for undefined values and set defaults. */ 
+chrome.storage.local.get(null, function(items) {
+	//webRTCIPHandlingPolicy 
+	if (items.rtcIPHandling == undefined) {
+		chrome.storage.local.set({
+			rtcIPHandling: 'default_public_interface_only'
+		}, function() {
+			chrome.privacy.network.webRTCIPHandlingPolicy.set({
+				value: 'default_public_interface_only'
+			});
+		})
+	}
+	//webRTCMultipleRoutesEnabled 
+	if (items.rtcMultipleRoutes == undefined) {
+		chrome.storage.local.set({
+			rtcMultipleRoutes: true
+		}, function() {
+			chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+				value: false,
+				scope: 'regular'
+			});
+		})
+	}
+	//Toggle status
+	if (items.toggleStatus == undefined) {
+		chrome.storage.local.set({
+			toggleStatus: true
+		});
+		chrome.browserAction.setIcon({
+			path: '/img/icon32_on.png'
+		});
+	}
 });
 
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-	try { //Uses webRTCMultipleRoutesEnabled for toggle status.
-		chrome.privacy.network.webRTCMultipleRoutesEnabled.get({}, function(setWebRTC) {
-			if (setWebRTC.value) { //On.
-				//webRTCMultipleRoutesEnabled set to false.
-				chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-					'value': false,
-					scope: 'regular'
-				});
-				//webRTCNonProxiedUdpEnabled set to user preference, same with webRTCIPHandlingPolicy.
-				chrome.storage.local.get(null, function(items) {
-					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-						value: !items.nonProxiedUDP,
-						scope: 'regular'
-					});
-					chrome.privacy.network.webRTCIPHandlingPolicy.set({
-						value: items.rtcIPHandling
-					});
-				});
-				chrome.browserAction.setIcon({
-					path: '/img/icon128_on.png'
-				});
-			} else { //Off.
-				//webRTCMultipleRoutesEnabled set to true.
-				chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-					'value': true,
-					scope: 'regular'
-				});
-
-				//webRTCNonProxiedUdpEnabled set to user preference. WebRTCIPHandlingPolicy set to 'default'.
-				chrome.storage.local.get(null, function(items) {
-					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-						value: items.nonProxiedUDP,
-						scope: 'regular'
-					});
+	try {
+		chrome.storage.local.get('toggleStatus', function(setWebRTC) {
+			if (setWebRTC.toggleStatus) {
+				chrome.storage.local.get('rtcIPHandling', function(items) {
 					chrome.privacy.network.webRTCIPHandlingPolicy.set({
 						value: 'default'
 					});
 				});
+				chrome.storage.local.get('rtcMultipleRoutes', function(items) {
+					chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+						value: true,
+						scope: 'regular'
+					});
+				});
+				chrome.storage.local.get('nonProxiedUDP', function(items) {
+					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+						value: true,
+						scope: 'regular'
+					});
+				});
 				chrome.browserAction.setIcon({
-					path: '/img/icon128_off.png'
+					path: '/img/icon32_off.png'
+				});
+				chrome.storage.local.set({
+					toggleStatus: false
+				});
+			} else {
+				chrome.storage.local.get('rtcIPHandling', function(items) {
+					chrome.privacy.network.webRTCIPHandlingPolicy.set({
+						value: items.rtcIPHandling
+					});
+				});
+				chrome.storage.local.get('rtcMultipleRoutes', function(items) {
+					chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+						value: !items.rtcMultipleRoutes,
+						scope: 'regular'
+					});
+				});
+				chrome.storage.local.get('nonProxiedUDP', function(items) {
+					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+						value: !items.nonProxiedUDP,
+						scope: 'regular'
+					});
+				});
+				chrome.browserAction.setIcon({
+					path: '/img/icon32_on.png'
+				});
+				chrome.storage.local.set({
+					toggleStatus: true
 				});
 			}
 		});
 	} catch (e) {
-		alert("An error occured. Is this a compatible browser? " + e);
+		alert("A toggle error occured. " + e);
 	}
 });
--- js/options.js
+++ js/options.js
@@ -1,22 +1,36 @@
 function save_options() {
 	var nonProxiedUDP = document.getElementById('proxy').checked;
 	var policy = document.getElementById('policy').value;
+	var rtcMultipleRoutes = document.getElementById('multipleroutes').checked;
 	chrome.storage.local.set({
 		nonProxiedUDP: nonProxiedUDP,
-		rtcIPHandling: policy
+		rtcIPHandling: policy,
+		rtcMultipleRoutes: rtcMultipleRoutes
 	}, function() {
 		var status = document.getElementById('status');
 		status.textContent = 'Options saved.';
-		//Set to user preference.
-		chrome.storage.local.get(null, function(items) {
-			chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-				value: !items.nonProxiedUDP,
-				scope: 'regular'
-			});
-			chrome.privacy.network.webRTCIPHandlingPolicy.set({
-				value: items.rtcIPHandling
-			});
-		}); //End.
+		/* Set to user preference on click if toggle is on. */
+		chrome.storage.local.get('toggleStatus', function(items) {
+			if (items.toggleStatus == true) {
+				chrome.storage.local.get('rtcIPHandling', function(items) {
+					chrome.privacy.network.webRTCIPHandlingPolicy.set({
+						value: items.rtcIPHandling
+					});
+				});
+				chrome.storage.local.get('rtcMultipleRoutes', function(items) {
+					chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+						value: !items.rtcMultipleRoutes,
+						scope: 'regular'
+					});
+				});
+				chrome.storage.local.get('nonProxiedUDP', function(items) {
+					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+						value: !items.nonProxiedUDP,
+						scope: 'regular'
+					});
+				});	
+			}
+		});
 		setTimeout(function() {
 			status.textContent = '';
 		}, 750);
@@ -26,10 +40,12 @@ function save_options() {
 function restore_options() {
 	chrome.storage.local.get({
 		nonProxiedUDP: false,
-		rtcIPHandling: 'default_public_interface_only'
+		rtcIPHandling: 'default_public_interface_only',
+		rtcMultipleRoutes: true
 	}, function(items) {
 		document.getElementById('proxy').checked = items.nonProxiedUDP;
 		document.getElementById('policy').value = items.rtcIPHandling;
+		document.getElementById('multipleroutes').checked = items.rtcMultipleRoutes;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
