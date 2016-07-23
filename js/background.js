69a733bd9a4e446988d5df0c9e021ec199f270dc
rentamob <rentamob@users.noreply.github.com>
2016-07-23 17:28:34 +1000

    Cleanup for legacy options.
    Legacy options will only be set if the browser supports the option/s.

--- js/background.js
+++ js/background.js
@@ -1,59 +1,10 @@
-/*	Set webRTCIPHandlingPolicy to 'disable_non_proxied_udp'
-	if user has previously selected the equivalent legacy 
-	option.
-	
-	This will prevent the default webRTCIPHandlingPolicy
-	option from overriding webRTCNonProxiedUdpEnabled: 
-	false.
-	
-	This will only run on update, and will only be
-	included in version 1.0.4. */
-chrome.runtime.onInstalled.addListener(function(details) {
-	if (details.reason == "update") {
-		chrome.storage.local.get('nonProxiedUDP', function(items) {
-			if (items.nonProxiedUDP == true) {
-				chrome.storage.local.set({
-					rtcIPHandling: 'disable_non_proxied_udp'
-				}, function() {
-					chrome.storage.local.get('toggleStatus', function(items) {
-						if (items.toggleStatus == true) {
-							chrome.privacy.network.webRTCIPHandlingPolicy.set({
-								value: 'disable_non_proxied_udp'
-							});
-						}
-					});
-				})
-			}
-		});
+function getMajorVerison(){
+	var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+	return raw ? parseInt(raw[2], 10) : false;
+}
 
-	}
-});
-
-/* Check for undefined values and set defaults. */ 
-chrome.storage.local.get(null, function(items) {
-	//webRTCIPHandlingPolicy 
-	if (items.rtcIPHandling == undefined) {
-		chrome.storage.local.set({
-			rtcIPHandling: 'default_public_interface_only'
-		}, function() {
-			chrome.privacy.network.webRTCIPHandlingPolicy.set({
-				value: 'default_public_interface_only'
-			});
-		})
-	}
-	//webRTCMultipleRoutesEnabled 
-	if (items.rtcMultipleRoutes == undefined) {
-		chrome.storage.local.set({
-			rtcMultipleRoutes: true
-		}, function() {
-			chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-				value: false,
-				scope: 'regular'
-			});
-		})
-	}
-	//Toggle status
-	if (items.toggleStatus == undefined) {
+chrome.storage.local.get(null, function(items){
+	if (items.toggleStatus == undefined){
 		chrome.storage.local.set({
 			toggleStatus: true
 		});
@@ -61,62 +12,108 @@ chrome.storage.local.get(null, function(items) {
 			path: '/img/icon32_on.png'
 		});
 	}
+	if(getMajorVerison() > 47){
+		if (items.rtcIPHandling == undefined){
+			chrome.storage.local.set({
+				rtcIPHandling: 'default_public_interface_only'
+			}, function(){
+				chrome.privacy.network.webRTCIPHandlingPolicy.set({
+					value: 'default_public_interface_only'
+				});
+			})
+		}
+	}
+	else if(getMajorVerison() > 41 && getMajorVerison() < 48){
+		if (items.rtcMultipleRoutes == undefined){
+			chrome.storage.local.set({
+				rtcMultipleRoutes: true
+			}, function(){
+				chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+					value: false,
+					scope: 'regular'
+				});
+			})
+		}
+	}
 });
 
-chrome.browserAction.onClicked.addListener(function(activeTab) {
-	try {
-		chrome.storage.local.get('toggleStatus', function(setWebRTC) {
-			if (setWebRTC.toggleStatus) {
-				chrome.storage.local.get('rtcIPHandling', function(items) {
-					chrome.privacy.network.webRTCIPHandlingPolicy.set({
-						value: 'default'
+chrome.browserAction.onClicked.addListener(function(activeTab){
+	try{
+		chrome.storage.local.get('toggleStatus', function(setWebRTC){
+			var passCheck = false;
+			if (setWebRTC.toggleStatus){
+				if(getMajorVerison() > 47){
+					chrome.storage.local.get('rtcIPHandling', function(items){
+						chrome.privacy.network.webRTCIPHandlingPolicy.set({
+							value: 'default'
+						});
 					});
-				});
-				chrome.storage.local.get('rtcMultipleRoutes', function(items) {
-					chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-						value: true,
-						scope: 'regular'
+					passCheck = true;
+				}
+				else if(getMajorVerison() > 41 && getMajorVerison() < 48){
+					chrome.storage.local.get('rtcMultipleRoutes', function(items) {
+						chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+							value: true,
+							scope: 'regular'
+						});
 					});
-				});
-				chrome.storage.local.get('nonProxiedUDP', function(items) {
-					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-						value: true,
-						scope: 'regular'
-					});
-				});
-				chrome.browserAction.setIcon({
+					if(getMajorVerison() == 47){
+						chrome.storage.local.get('nonProxiedUDP', function(items){
+							chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+								value: true,
+								scope: 'regular'
+							});
+						});
+					}
+					passCheck = true;
+				}
+				if(passCheck){
+					chrome.browserAction.setIcon({
 					path: '/img/icon32_off.png'
-				});
-				chrome.storage.local.set({
-					toggleStatus: false
-				});
-			} else {
-				chrome.storage.local.get('rtcIPHandling', function(items) {
-					chrome.privacy.network.webRTCIPHandlingPolicy.set({
-						value: items.rtcIPHandling
 					});
-				});
-				chrome.storage.local.get('rtcMultipleRoutes', function(items) {
-					chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-						value: !items.rtcMultipleRoutes,
-						scope: 'regular'
+					chrome.storage.local.set({
+						toggleStatus: false
 					});
-				});
-				chrome.storage.local.get('nonProxiedUDP', function(items) {
-					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-						value: !items.nonProxiedUDP,
-						scope: 'regular'
+				}
+			} 
+			else{
+				if(getMajorVerison() > 47){
+					chrome.storage.local.get('rtcIPHandling', function(items){
+						chrome.privacy.network.webRTCIPHandlingPolicy.set({
+							value: items.rtcIPHandling
+						});
 					});
-				});
-				chrome.browserAction.setIcon({
-					path: '/img/icon32_on.png'
-				});
-				chrome.storage.local.set({
-					toggleStatus: true
-				});
+					passCheck = true;
+				}
+				else if(getMajorVerison() > 41 && getMajorVerison() < 48){
+					chrome.storage.local.get('rtcMultipleRoutes', function(items){
+						chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+							value: !items.rtcMultipleRoutes,
+							scope: 'regular'
+						});
+					});
+					if(getMajorVerison() == 47){
+						chrome.storage.local.get('nonProxiedUDP', function(items){
+							chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+								value: !items.nonProxiedUDP,
+								scope: 'regular'
+							});
+						});
+					}
+					passCheck = true;
+				}
+				if(passCheck){
+					chrome.browserAction.setIcon({
+						path: '/img/icon32_on.png'
+					});
+					chrome.storage.local.set({
+						toggleStatus: true
+					});
+				}
 			}
 		});
-	} catch (e) {
-		alert("A toggle error occured. " + e);
+	} 
+	catch (e){
+		console.log("Error: " + e + ". Browser version: " + getMajorVerison());
 	}
 });
--- js/options.js
+++ js/options.js
@@ -1,53 +1,155 @@
-function save_options() {
-	var nonProxiedUDP = document.getElementById('proxy').checked;
-	var policy = document.getElementById('policy').value;
-	var rtcMultipleRoutes = document.getElementById('multipleroutes').checked;
-	chrome.storage.local.set({
-		nonProxiedUDP: nonProxiedUDP,
-		rtcIPHandling: policy,
-		rtcMultipleRoutes: rtcMultipleRoutes
-	}, function() {
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		/* Set to user preference on click if toggle is on. */
-		chrome.storage.local.get('toggleStatus', function(items) {
-			if (items.toggleStatus == true) {
-				chrome.storage.local.get('rtcIPHandling', function(items) {
-					chrome.privacy.network.webRTCIPHandlingPolicy.set({
-						value: items.rtcIPHandling
-					});
-				});
-				chrome.storage.local.get('rtcMultipleRoutes', function(items) {
-					chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
-						value: !items.rtcMultipleRoutes,
-						scope: 'regular'
-					});
-				});
-				chrome.storage.local.get('nonProxiedUDP', function(items) {
-					chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
-						value: !items.nonProxiedUDP,
-						scope: 'regular'
-					});
-				});	
-			}
-		});
-		setTimeout(function() {
-			status.textContent = '';
-		}, 750);
-	});
+function getMajorVerison(){
+	var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+	return raw ? parseInt(raw[2], 10) : false;
 }
 
-function restore_options() {
-	chrome.storage.local.get({
-		nonProxiedUDP: false,
-		rtcIPHandling: 'default_public_interface_only',
-		rtcMultipleRoutes: true
-	}, function(items) {
-		document.getElementById('proxy').checked = items.nonProxiedUDP;
-		document.getElementById('policy').value = items.rtcIPHandling;
-		document.getElementById('multipleroutes').checked = items.rtcMultipleRoutes;
-	});
+function displayContent(){
+	var divContent = document.getElementById('content');
+	var divNew = document.getElementById('new');
+	var divLegacyRoutes = document.getElementById('legacyRoutes');
+	var divLegacyProxy = document.getElementById('legacyProxy');
+	var divFail = document.getElementById('fail');
+	var divIncognito = document.getElementById('incognito');
+	var divApply = document.getElementById('applyButton');
+	
+	if(getMajorVerison() > 47){
+		divLegacyProxy.style.display = 'none';
+		divLegacyRoutes.style.display = 'none';
+		divFail.style.display = 'none';
+		chrome.extension.isAllowedIncognitoAccess();
+	}
+	else if(getMajorVerison() > 41 && getMajorVerison() < 47){
+		divNew.style.display = 'none';
+		divFail.style.display = 'none';
+		divLegacyProxy.style.display = 'none';
+		chrome.extension.isAllowedIncognitoAccess();
+	}
+	else if(getMajorVerison() == 47){
+		divNew.style.display = 'none';
+		divFail.style.display = 'none';
+		chrome.extension.isAllowedIncognitoAccess();
+	}
+	else{
+		divContent.style.display = 'none';
+		divIncognito.style.display = 'none';
+		divApply.style.display = 'none';
+	}
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-	save_options);
+
+chrome.extension.isAllowedIncognitoAccess(function(isAllowedAccess){
+	var incognitoAllowed = document.getElementById('incognitoAllowed');
+	var incognitoDisallowed = document.getElementById('incognitoDisallowed');
+	
+	if(isAllowedAccess == true){
+		incognitoDisallowed.style.display = 'none';
+	}
+	else{
+		incognitoAllowed.style.display = 'none';
+	}
+});
+
+function saveOptions(){
+	if(getMajorVerison() > 47){
+		var policy = document.getElementById('policy').value;
+		chrome.storage.local.set({
+			rtcIPHandling: policy
+		}, function(){
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			chrome.storage.local.get('toggleStatus', function(items) {
+				if (items.toggleStatus){
+					chrome.storage.local.get('rtcIPHandling', function(items){
+						chrome.privacy.network.webRTCIPHandlingPolicy.set({
+							value: items.rtcIPHandling
+						});
+					});
+				}
+			});
+			setTimeout(function(){
+				status.textContent = '';
+			}, 750);
+		});
+	}
+	else if(getMajorVerison() == 47){
+		var nonProxiedUDP = document.getElementById('proxy').checked;
+		var rtcMultipleRoutes = document.getElementById('multipleroutes').checked;
+		chrome.storage.local.set({
+			nonProxiedUDP: nonProxiedUDP,
+			rtcMultipleRoutes: rtcMultipleRoutes
+		}, function(){
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			chrome.storage.local.get('toggleStatus', function(items) {
+				if (items.toggleStatus){
+					chrome.storage.local.get('rtcMultipleRoutes', function(items){
+						chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+							value: !items.rtcMultipleRoutes,
+							scope: 'regular'
+						});
+					});
+					chrome.storage.local.get('nonProxiedUDP', function(items){
+						chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({
+							value: !items.nonProxiedUDP,
+							scope: 'regular'
+						});
+					});
+				}
+			});
+			setTimeout(function(){
+				status.textContent = '';
+			}, 750);
+		});
+	}
+	else if(getMajorVerison() > 41 && getMajorVerison() < 47){
+		var rtcMultipleRoutes = document.getElementById('multipleroutes').checked;
+		chrome.storage.local.set({
+			rtcMultipleRoutes: rtcMultipleRoutes
+		}, function(){
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			chrome.storage.local.get('toggleStatus', function(items) {
+				if (items.toggleStatus){
+					chrome.storage.local.get('rtcMultipleRoutes', function(items){
+						chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
+							value: !items.rtcMultipleRoutes,
+							scope: 'regular'
+						});
+					});
+				}
+			});
+			setTimeout(function(){
+				status.textContent = '';
+			}, 750);
+		});
+	}
+}
+
+function restoreOptions(){
+	if(getMajorVerison() > 47){
+		chrome.storage.local.get({
+			rtcIPHandling: 'default_public_interface_only'
+		}, function(items){
+			document.getElementById('policy').value = items.rtcIPHandling;
+		});
+	}
+	else if(getMajorVerison() == 47){
+		chrome.storage.local.get({
+			nonProxiedUDP: false,
+			rtcMultipleRoutes: true
+		}, function(items){
+			document.getElementById('proxy').checked = items.nonProxiedUDP;
+			document.getElementById('multipleroutes').checked = items.rtcMultipleRoutes;
+		});
+	}
+	else if(getMajorVerison() > 41 && getMajorVerison() < 47){
+		chrome.storage.local.get({
+			rtcMultipleRoutes: true
+		}, function(items){
+			document.getElementById('multipleroutes').checked = items.rtcMultipleRoutes;
+		});
+	}
+}
+
+document.addEventListener('DOMContentLoaded', displayContent);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
